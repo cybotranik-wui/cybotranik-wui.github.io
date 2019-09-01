1555e3acbf3ec5330c75508753bf1ebfe6913c3b
Azmi ŞAHİN <azmisahin@outlook.com>
2019-09-01 17:41:04 +0300

    offline

---
+++ service-worker.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var cacheVersion = 1;
+var currentCache = {
+  offline: 'offline-cache' + cacheVersion
+};
+const offlineUrl = 'cache.html';
+
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache.offline).then(function (cache) {
+      return cache.addAll([
+        './sw.js',
+        './service-worker.js',
+        './cache.html',
+        './fallback.html',
+        './index.html',
+        './docs/media/favicon.ico',
+        './docs/media/favicon.png',
+        offlineUrl
+      ]);
+    })
+  );
+});
+
+// Cache any new resources as they are fetched
+self.addEventListener('fetch', function (event) {
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function (response) {
+            if (!response || response.status !== 200) {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+            caches.open(cacheName)
+              .then(function (cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        ).catch(error => {
+
+          if (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html')) {
+
+            return caches.match(offlineUrl);
+          }
+        });
+      })
+  );
+
+});
