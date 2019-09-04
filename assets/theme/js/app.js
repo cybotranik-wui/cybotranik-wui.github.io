6ca4da58359d86e07ea500aa344b7228fd4e10a9
Azmi ŞAHİN <azmisahin@outlook.com>
2019-09-04 12:30:54 +0300

    service worker

--- assets/theme/js/app.js
+++ assets/theme/js/app.js
@@ -1,5 +1,5 @@
-﻿/**
- * Cybotranik WUI
+/**
+ * Cybotranik WUI Documentation
  *
  * Website html User Interface.
  * 
@@ -8,3 +8,37 @@
  * @author Azmi SAHIN
  * @since 2019
  * */
+function Documentation() { }
+
+/**
+ * PWA Application
+ */
+Documentation.prototype.ServiceWorker = function () {
+
+  /// Service Worker Cache Delete
+  function deleteCache(){
+    caches.keys().then(function(names) {
+      for (let name of names){
+        console.log('Cache Clear : ' + name)
+        caches.delete(name)
+      }
+    })
+  }
+
+  if ('serviceWorker' in navigator) {
+
+    // deleteCache()
+
+    navigator
+      .serviceWorker.register('service-worker.js', { scope: '/' })
+      .then(function () {
+        
+      })
+
+    navigator.serviceWorker.ready.then(function () {
+      
+    })
+  }
+}
+
+new Documentation().ServiceWorker()
--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,29 @@
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', { scope: '/' })
-    .then(function(registration) {
-          console.log('Service Worker Registered');
-    });
+var version = '0.0.1'
+var cacheName = 'cybotranik-wui-' + version
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/'
+      ])
+        .then(() => self.skipWaiting())
+    })
+  )
+})
 
-  navigator.serviceWorker.ready.then(function(registration) {
-     console.log('Service Worker Ready');
-  });
-}
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    self.clients.claim()
+  )
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+        return response || fetch(event.request)
+      })
+  )
+})
+
